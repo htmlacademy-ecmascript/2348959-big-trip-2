4aec59792a43a1e327c5e6b6ee80b41c89d929ab
dklobov <d.k.lobov@gmail.com>
2026-05-08 22:56:16 +0300

    добавляет конфигурацию Webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+  new HtmlWebpackPlugin({
+    template: './public/index.html',
+  }),
+  new CopyPlugin({
+    patterns: [
+      {
+        from: 'public',
+        globOptions: {
+          ignore: ['**/index.html'],
+        },
+      },
+    ],
+  }),
+],
+};
