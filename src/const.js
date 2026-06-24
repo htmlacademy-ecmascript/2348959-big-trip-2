3449c70693f94beba6b3568c18492fdb44f648bd
dklobov <d.k.lobov@gmail.com>
2026-06-24 19:33:39 +0300

    Добавляет типы обновлений и действий пользователя

--- src/const.js
+++ src/const.js
@@ -32,4 +32,16 @@ const NoPointTextType = {
   [FilterType.PAST]: 'There are no past events now',
 };
 
-export {DESCRIPTION_CITIES, FilterType, NoPointTextType, SortType};
+const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
+const UserAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
+};
+
+export {DESCRIPTION_CITIES, FilterType, NoPointTextType, SortType, UpdateType, UserAction};
