08629aed743c524b98f69f5525d22a352aa77707
dklobov <d.k.lobov@gmail.com>
2026-06-30 23:05:32 +0300

    Создает API-сервис и запускает загрузку данных

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import TripPresenter from './presenter/trip-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import TripModel from './model/trip-model.js';
 import FilterModel from './model/filter-model.js';
+import TripApiService from './trip-api-service.js';
 
-const tripModel = new TripModel();
+const AUTHORIZATION = 'Basic qwerty12345';
+const END_POINT = 'https://22.objects.htmlacademy.pro/big-trip';
+
+const tripApiService = new TripApiService(END_POINT, AUTHORIZATION);
+
+const tripModel = new TripModel({
+  tripApiService,
+});
 const filterModel = new FilterModel();
 
 const tripPresenter = new TripPresenter({
@@ -19,3 +27,4 @@ const filterPresenter = new FilterPresenter({
 
 filterPresenter.init();
 tripPresenter.init();
+tripModel.init();
