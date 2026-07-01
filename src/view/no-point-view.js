963f37e76bc5dcdf170540820e3ba3ae2ded8cce
dklobov <d.k.lobov@gmail.com>
2026-07-01 13:31:58 +0300

    Экранирует текст заглушки списка

--- src/view/no-point-view.js
+++ src/view/no-point-view.js
@@ -1,5 +1,6 @@
-import AbstractView from '../framework/view/abstract-view.js';
 import {NoPointTextType} from '../const.js';
+import {escapeHtml} from '../utils/common.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 export default class NoPointView extends AbstractView {
   #filterType = null;
@@ -12,7 +13,8 @@ export default class NoPointView extends AbstractView {
 
   get template() {
     const noPointText = NoPointTextType[this.#filterType];
+    const escapedNoPointText = escapeHtml(noPointText);
 
-    return `<p class="trip-events__msg">${noPointText}</p>`;
+    return `<p class="trip-events__msg">${escapedNoPointText}</p>`;
   }
 }
