85dc282b24e673b456af6d0cfa683f94edfb0a1f
dklobov <d.k.lobov@gmail.com>
2026-07-01 13:32:47 +0300

    Добавляет экранирование HTML-строк

---
+++ src/utils/common.js
@@ -0,0 +1,8 @@
+const escapeHtml = (value) => String(value)
+  .replaceAll('&', '&amp;')
+  .replaceAll('<', '&lt;')
+  .replaceAll('>', '&gt;')
+  .replaceAll('"', '&quot;')
+  .replaceAll('\'', '&#39;');
+
+export {escapeHtml};
