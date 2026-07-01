08c35f30b114f1c92bbd29ed65ce790e3509a12d
dklobov <d.k.lobov@gmail.com>
2026-07-01 13:31:48 +0300

    Экранирует данные информации о путешествии

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,3 +1,4 @@
+import {escapeHtml} from '../utils/common.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 export default class TripInfoView extends AbstractView {
@@ -14,16 +15,20 @@ export default class TripInfoView extends AbstractView {
   }
 
   get template() {
+    const escapedTitle = escapeHtml(this.#title);
+    const escapedDates = escapeHtml(this.#dates);
+    const escapedCost = escapeHtml(this.#cost);
+
     return `
       <section class="trip-main__trip-info  trip-info">
         <div class="trip-info__main">
-          <h1 class="trip-info__title">${this.#title}</h1>
+          <h1 class="trip-info__title">${escapedTitle}</h1>
 
-          <p class="trip-info__dates">${this.#dates}</p>
+          <p class="trip-info__dates">${escapedDates}</p>
         </div>
 
         <p class="trip-info__cost">
-          Total: &euro;&nbsp;<span class="trip-info__cost-value">${this.#cost}</span>
+          Total: &euro;&nbsp;<span class="trip-info__cost-value">${escapedCost}</span>
         </p>
       </section>
     `;
